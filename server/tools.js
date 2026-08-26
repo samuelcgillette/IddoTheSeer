072623f0c63e822e842370fd46a350ccc01ffbd9
Samuel <samuelcgillette@gmail.com>
2026-08-25 19:57:32 -0600

    page number tool added for individual books

--- server/tools.js
+++ server/tools.js
@@ -86,16 +86,36 @@ export const getAbriviationTool = tool(
 );
 
 export const getBookText = tool(
-    async ({ bookAbriviation }) => {
+    async ({ bookAbriviation, pageNumber }) => {
         console.log(`getting ${bookAbriviation}`)
         const parser = new PDFParse({ url: `https://ebible.org/pdf/eng-kjv2006/eng-kjv2006_${bookAbriviation}.pdf` });
-        return await parser.getText();
+        text = await parser.getText({partial: [pageNumber]});
+        parser.destroy();
+        return text;
     },
     {
         name: "get_book_text",
         description: "Get the text of a bible book using the abreviation from get_abriviation_tool. Do not insert a book abreviation that is not from the get_abriviation_tool.",
         schema: z.object({
-            bookAbriviation: z.string().describe("The abreviation recived from get_abriviation_tool. It must be all caps and the correct abreviation for the book you are trying to get the text for. ex MAT for Matthew, JHN for John, etc. Exact abreviations are found in the get_abriviation_tool.")
+            bookAbriviation: z.string().describe("The abreviation recived from get_abriviation_tool. It must be all caps and the correct abreviation for the book you are trying to get the text for. ex MAT for Matthew, JHN for John, etc. Exact abreviations are found in the get_abriviation_tool."),
+            pageNumber: z.number().describe("The page number of the book you want to get the text for. The page number is the page number in the PDF document. It is not the chapter or verse number. The page number is used to get a specific section of the book text. If you want to progress through the entire book text you can call this tool multiple times with incrementing page numbers until the desire result is found. Start at one and increment. This number can not be greater than the total number of pages in the book.")
+        }),
+    }
+);
+
+export const getNumberOfPages = tool(
+    async ({ bookAbriviation }) => {
+        console.log(`getting number of pages for ${bookAbriviation}`)
+        const parser = new PDFParse({ url: `https://ebible.org/pdf/eng-kjv2006/eng-kjv2006_${bookAbriviation}.pdf` });
+        const result = await parser.getInfo({ parsePageInfo: true }); 
+        parser.destroy();
+        return result.total;
+    },
+    {
+        name: "get_number_of_pages",
+        description: "Get the number of pages in a bible book using the abreviation from get_abriviation_tool. Do not insert a book abreviation that is not from the get_abriviation_tool.",
+        schema: z.object({
+            bookAbriviation: z.string().describe("The abreviation recived from get_abriviation_tool. It must be all caps and the correct abreviation for the book you are trying to get the text for. ex MAT for Matthew, JHN for John, etc. Exact abreviations are only found in the get_abriviation_tool.")
         }),
     }
 );
